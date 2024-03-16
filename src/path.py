ff4d4018b9c7b23a542efd18da38c9a404e4999c
Giulia Carra <giulia.carra@gmail.com>
2024-03-16 15:32:17 +0100

    formatted path file

--- src/path.py
+++ src/path.py
@@ -3,8 +3,7 @@ from pathlib import Path
 import os
 
 PARENT_DIR = Path(__file__).parent.resolve().parent
-DATA_DIR = PARENT_DIR / 'data'
-
+DATA_DIR = PARENT_DIR / "data"
 
 if not Path(DATA_DIR).exists():
     os.mkdir(DATA_DIR)
