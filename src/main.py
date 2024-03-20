13d9cc9fa398604ebb44e91bed80d40946794edf
Giulia Carra <giulia.carra@gmail.com>
2024-03-20 18:15:38 +0100

    src main formatting modified

--- src/main.py
+++ src/main.py
@@ -8,23 +8,24 @@ def display_results(test_mae, mean_test_count, train_mae, mean_train_count):
     Function displaying results
     """
     print(f"The mean absolute error of the test set is {test_mae:.2f}")
-    print(f"The average hourly number of users of the test set is {mean_test_count:.2f}")
+    print(f"The average hourly number of users of the test set is {mean_test_count:.2f}\n")
 
     print(f"The mean absolute error of the train set is {train_mae:.2f}")
-    print(f"The average hourly number of users of the train set is {mean_train_count:.2f}")
+    print(f"The average hourly number of users of the train set is {mean_train_count:.2f}\n")
 
 if __name__ == "__main__":
 
     raw_data = pd.read_csv(DATA_DIR / "raw_data.csv") 
 
     test_mae, mean_test_count, train_mae, mean_train_count = evaluate_Baseline(raw_data)
-    print('Baseline model')
+    print('\n Baseline model')
     display_results(test_mae, mean_test_count, train_mae, mean_train_count)
+    print("\n")
 
     test_mae, mean_test_count, train_mae, mean_train_count = evaluate_xgb(raw_data)
-    print('XGBoost model')
+    print('\n XGBoost model')
     display_results(test_mae, mean_test_count, train_mae, mean_train_count)
 
     test_mae, mean_test_count, train_mae, mean_train_count = evaluate_LGBM(raw_data, force_col_wise = True)
-    print('LightGBM model')
+    print('\n LightGBM model')
     display_results(test_mae, mean_test_count, train_mae, mean_train_count)
